b53b18cacccd535ca72918ea98f775e993639169
Mike <jkellymichael@gmail.com>
2020-11-10 19:51:03 +0100

    add loading component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import Tours from "./components/Tours";
 import "./sass/style.scss";
+import axios from "axios";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [tour, setTour] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTour = () => {
+    setLoading(true);
+    try {
+      axios.get(url).then((res) => {
+        setTour(res.data);
+        setLoading(false);
+      });
+    } catch (error) {
+      setLoading(false);
+      console.log("error :>> ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTour();
+  }, []);
+
   return (
     <main className="App">
-      <h1>Nos tour</h1>
+      <h1>Nos Sejours</h1>
       <Tours />
     </main>
   );

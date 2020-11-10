ae282f26ecf7eb994a3c32dc012f3dd617f65f25
Mike <jkellymichael@gmail.com>
2020-11-10 19:17:02 +0100

    add style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Tours from "./components/Tours";
+import "./sass/style.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>Nos tour</h1>
+      <Tours />
+    </main>
   );
 }
 

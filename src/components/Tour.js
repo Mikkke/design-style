b72bdffa4a473720e574126d3658c48e2f2d52d0
Mike <jkellymichael@gmail.com>
2020-11-11 19:09:23 +0100

    update tour component

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Tour = () => {
-  return <div></div>;
+const Tour = ({ id, name, image, info, price }) => {
+  return (
+    <article>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <h4>{price}</h4>
+      <p>{info}</p>
+    </article>
+  );
 };
 
 export default Tour;

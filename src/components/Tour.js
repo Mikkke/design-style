776a17725d1b2f161541fd9b760a1a09478bd67d
Mike <jkellymichael@gmail.com>
2020-11-11 19:20:54 +0100

    add readmore button

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Tour = ({ id, name, image, info, price }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article>
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <h4>{price}</h4>
-      <p>{info}</p>
+      <p>
+        {readMore ? info : `${info.substring(0, 200)}...`}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? `reduire` : `en savoir plus`}
+        </button>
+      </p>
     </article>
   );
 };
